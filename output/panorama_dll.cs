37e1832e9b7b01cce881b41bbc2b8560d8d36d39
Darknet1337666 <kyurkchyud@gmail.com>
2025-11-05 03:42:36 +0300

    05.11.25
    05.11.25

---
+++ output/panorama_dll.cs
@@ -0,0 +1,14 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-11-05 00:34:37.341840100 UTC
+
+namespace CS2Dumper.Schemas {
+    // Module: panorama.dll
+    // Class count: 1
+    // Enum count: 0
+    public static class PanoramaDll {
+        // Parent: None
+        // Field count: 0
+        public static class ELayoutNodeType {
+        }
+    }
+}
---
+++ output/panorama_dll.hpp
@@ -0,0 +1,20 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-11-05 00:34:37.341840100 UTC
+
+#pragma once
+
+#include <cstddef>
+
+namespace cs2_dumper {
+    namespace schemas {
+        // Module: panorama.dll
+        // Class count: 1
+        // Enum count: 0
+        namespace panorama_dll {
+            // Parent: None
+            // Field count: 0
+            namespace ELayoutNodeType {
+            }
+        }
+    }
+}
